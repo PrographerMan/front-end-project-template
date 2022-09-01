478adb16f546526eea6463d982b245c68381b746
PrographerMan <morozov.salevii@gmail.com>
2022-09-02 03:54:13 +0500

    Правки

--- gulpfile.js
+++ gulpfile.js
@@ -12,28 +12,35 @@ const babel = require('gulp-babel');
 const eslint = require('gulp-eslint');
 const sourcemaps = require('gulp-sourcemaps');
 
+const BUILD_PREFIX = 'b';
 
-const cssBundleFileName = 'bundle.min.css';
-const jsBundleFilename = 'bundle.min.js';
+const CSS_BUNDLE_FILE_NAME = 'bundle.min.css';
+const JS_BUNDLE_FILE_NAME = 'bundle.min.js';
 
-const Source = {
-  html:      'app/**/*.html',
-  css:       'app/template/css/*.+(scss|css)',
-  js:        'app/template/js/*.js',
-  images:    'app/template/img/**/*',
-  fonts:     'app/template/fonts/**/*',
-  jquery:    'node_modules/jquery/dist/jquery.min.js',
-  normalize: 'node_modules/normalize.css/normalize.css'
+const SOURCE = {
+  HTML:      'app/**/*.html',
+  CSS:       'app/template/css/*.+(scss|css)',
+  JS:        'app/template/js/*.js',
+  IMAGES:    'app/template/img/**/*',
+  FONTS:     'app/template/fonts/**/*',
+  JQUERY:    'node_modules/jquery/dist/jquery.min.js',
+  NORMALIZE: 'node_modules/normalize.css/normalize.css'
 }
 
-const Output = {
-  html:      'dist',
-  css:       'dist/template/css',
-  js:        'dist/template/js',
-  images:    'dist/template/img',
-  fonts:     'dist/template/fonts',
-  jquery:    'app/template/js',
-  normalize: 'app/template/js'
+const OUTPUT = {
+  HTML:      'dist',
+  CSS:       'dist/template/css',
+  JS:        'dist/template/js',
+  IMAGES:    'dist/template/img',
+  FONTS:     'dist/template/fonts',
+  JQUERY:    'app/template/js',
+  NORMALIZE: 'app/template/css'
+}
+
+const WATCH = {
+  HTML: 'app/**/*.html',
+  CSS: 'app/template/css/*.scss',
+  JS: 'app/template/js/*.js'
 }
 
 const imagesBuildConfig = {
@@ -53,65 +60,67 @@ const jsminifyConfig = {
   noSource: true
 };
 
-gulp.task('html:build', () => {
-  return gulp.src(Source.html)
+const BabelConfig = {
+  presets: ['@babel/env']
+}
+
+gulp.task(`html:${BUILD_PREFIX}`, () => {
+  return gulp.src(SOURCE.HTML)
     .pipe(rigger())
-    .pipe(gulp.dest(Output.html))
+    .pipe(gulp.dest(OUTPUT.HTML))
     .pipe(browserSync.reload({
       stream: true
     }))
 });
 
-gulp.task('css:build', () => {
-  return gulp.src(Source.css)
-    .pipe(concat(cssBundleFileName))
+gulp.task(`css:${BUILD_PREFIX}`, () => {
+  return gulp.src(SOURCE.CSS)
+    .pipe(concat(CSS_BUNDLE_FILE_NAME))
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(autoprefixer(autoprefixerConfig))
     .pipe(cleancss())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(Output.css))
+    .pipe(gulp.dest(OUTPUT.CSS))
     .pipe(browserSync.reload({
       stream: true
     }))
 });
 
-gulp.task('js:build', () => {
-  return gulp.src(Source.js)
-    .pipe(concat(jsBundleFilename))
+gulp.task(`js:${BUILD_PREFIX}`, () => {
+  return gulp.src(SOURCE.JS)
+    .pipe(concat(JS_BUNDLE_FILE_NAME))
     .pipe(eslint())
     .pipe(sourcemaps.init())
-    .pipe(babel({
-      presets: ['@babel/env']
-    }))
+    .pipe(babel(BabelConfig))
     .pipe(jsminify(jsminifyConfig))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(Output.js))
+    .pipe(gulp.dest(OUTPUT.JS))
     .pipe(browserSync.reload({
       stream: true
     }))
 });
 
-gulp.task('img:build', () => {
-  return gulp.src(Source.images)
+gulp.task(`images:${BUILD_PREFIX}`, () => {
+  return gulp.src(SOURCE.IMAGES)
     .pipe(imgmin(imagesBuildConfig))
-    .pipe(gulp.dest(Output.images));
+    .pipe(gulp.dest(OUTPUT.IMAGES));
 });
 
-gulp.task('fonts:build', () => {
-  return gulp.src(Source.fonts)
-    .pipe(gulp.dest(Output.fonts));
+gulp.task(`fonts:${BUILD_PREFIX}`, () => {
+  return gulp.src(SOURCE.FONTS)
+    .pipe(gulp.dest(OUTPUT.FONTS));
 });
 
 gulp.task('copy', () => {
-  gulp.src(Source.jquery).pipe(gulp.dest(Output.jquery));
-  gulp.src(Source.normalize).pipe(gulp.dest(Output.normalize));
+  gulp.src(SOURCE.JQUERY).pipe(gulp.dest(OUTPUT.JQUERY));
+  gulp.src(SOURCE.NORMALIZE).pipe(gulp.dest(OUTPUT.NORMALIZE));
 });
 
-gulp.task('watch', ['browserSync', 'copy', 'html:build', 'css:build', 'js:build'], () => {
-  gulp.watch('app/**/*.html', ['html:build']);
-  gulp.watch('app/template/css/*.scss', ['css:build']);
-  gulp.watch('app/template/js/*.js', ['js:build']);
+gulp.task('watch', ['browserSync', 'copy', `html:${BUILD_PREFIX}`, `css:${BUILD_PREFIX}`, `js:${BUILD_PREFIX}`], () => {
+  gulp.watch(WATCH.HTML, [`html:${BUILD_PREFIX}`]);
+  gulp.watch(WATCH.CSS, [`css:${BUILD_PREFIX}`]);
+  gulp.watch(WATCH.JS, [`js:${BUILD_PREFIX}`]);
 });
 
 gulp.task('browserSync', () => {
